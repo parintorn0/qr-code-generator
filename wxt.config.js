724f09d61da26efa7b0ec871af9a940c672819a3
parintorn0 <159538649+parintorn0@users.noreply.github.com>
2025-06-02 03:54:38 +0700

    added src folder and remove unnessesary permissions

--- wxt.config.js
+++ wxt.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-  // srcDir: 'src',
+  srcDir: 'src',
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react', '@wxt-dev/auto-icons'],
   manifest: {
     name: "QR Code Generator",
     description: "Create QR Code to sharing everywhere",
-    permissions: [
-      "activeTab",
-    ],
+    permissions: []
   },
+  
 });
